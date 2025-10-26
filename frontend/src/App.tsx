c5ea07fab0df4bb79814723b206dbb45dd24d9c1
zzeric <ecasane@edu.tecnocampus.cat>
2025-10-26 10:49:15 +0100

    PRT-2 style(VideoCard): update styling and layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,8 @@ import VideoGrid from './components/VideoGrid.tsx';
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <header className="w-full min-h-screen bg-gray-950 flex flex-col items-center justify-center text-white text-center px-4">
-        <img
-          src="/protube-logo-removebg-preview.png"
-          className="h-[40vmin] pointer-events-none"
-          alt="logo"
-        />
+      <header className="w-full min-h-screen bg-slate-900 flex flex-col items-center justify-center text-white text-center px-4">
+        <img src="/protube-logo-removebg-preview.png" className="h-[40vmin] pointer-events-none" alt="logo" />
         <ContentApp />
       </header>
     </div>
@@ -17,7 +13,7 @@ function App() {
 }
 
 function ContentApp() {
-  const { loading, message, value } = useAllVideos();
+  const { loading, message } = useAllVideos();
   switch (loading) {
     case 'loading':
       return <div className="text-lg mt-8">Loading...</div>;
@@ -32,14 +28,6 @@ function ContentApp() {
       return (
         <div className="mt-8">
           <VideoGrid />
-          <strong className="text-xl block mb-4">Videos available:</strong>
-          <ul className="space-y-2 list-disc list-inside">
-            {value.map((item) => (
-              <li key={item.videoFileName} className="text-lg">
-                {item.title}
-              </li>
-            ))}
-          </ul>
         </div>
       );
   }
--- frontend/src/components/VideoCard.tsx
+++ frontend/src/components/VideoCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 interface VideoCardProps {
   title: string;
@@ -6,16 +6,11 @@ interface VideoCardProps {
 }
 
 export const VideoCard: React.FC<VideoCardProps> = ({ title, imageUrl }) => (
-  <div className="rounded-lg shadow-sm overflow-hidden flex flex-col transition hover:shadow-md focus:outline-none w-full bg-neutral-800/60">
-    {/* CardBody */}
+  <div className="rounded-lg shadow-sm p-2 overflow-hidden flex flex-col transition hover:shadow-md focus:outline-none w-full bg-slate-800/60">
     <div className="overflow-visible p-0">
-      <img
-        src={imageUrl}
-        alt={title}
-        className="w-full object-cover rounded-lg shadow-sm"
-      />
+      <img src={imageUrl} alt={title} className="w-full h-[200px] object-cover rounded-lg shadow-sm" />
     </div>
-    {/* CardFooter */}
+
     <div className="flex items-center px-4 py-2 text-sm">
       <b className="truncate">{title}</b>
     </div>
